3a1eb27d9c070f8e0f72c8a5301782d369b5c019
JD <jdbriones22@gmail.com>
2021-09-15 19:17:26 +1200

    Blueprint refactor, routing, browse page
    finished implementation of blueprints, routing and started browse page

--- library/__init__.py
+++ library/__init__.py
@@ -20,10 +20,18 @@ def create_some_book():
 def create_app():
     app = Flask(__name__)
 
-    @app.route('/')
-    def home():
-        some_book = create_some_book()
-        # Use Jinja to customize a predefined html page rendering the layout for showing a single book.
-        return render_template('simple_book.html', book=some_book)
+    with app.app_context():
+        # Register blueprints.
+        from .home import home
+        app.register_blueprint(home.home_blueprint)
+
+        from .browse import browse
+        app.register_blueprint(browse.browse_blueprint)
+
+        # TODO: Authentication implementation
+        # from .authentication import authentication
+        # app.register_blueprint(authentication.authentication_blueprint)
+
+
 
     return app
