f35f3ff34c729176100a4dc995b770affbbe9766
burntmilk <lukey1321@hotmail.com>
2021-09-22 01:22:59 +1200

    finished app integration testing for authentication. still need to complete review tests

--- tests/e2e/test_web_app.py
+++ tests/e2e/test_web_app.py
@@ -29,3 +29,69 @@ def test_book(client):
     assert response.status_code == 200
     assert b'Cruelle' in response.data
 
+
+def test_review(client, auth):  # TODO: finish off review tests
+    auth.login()
+    response = client.get('/review/30128855')
+    pass
+
+
+def test_register(client):
+    response_code = client.get('/authentication/register').status_code
+    assert response_code == 200
+    response = client.post(
+        '/authentication/register',
+        data={'user_name': 'gmichael', 'password': 'CarelessWhisper1984'}
+    )
+    assert response.headers['Location'] == 'http://localhost/authentication/login'
+
+
+@pytest.mark.parametrize(('user_name', 'password', 'message'), (
+        ('', '', b'Your user name is required'),
+        ('cj', '', b'Your user name is too short'),
+        ('test', '', b'Your password is required'),
+        ('test', 'test', b'Your password must be at least 8 characters, and contain an upper case letter,\
+            a lower case letter and a digit'),
+        ('fmercury', 'Test#6^0', b'Your user name is already taken - please supply another'),
+))
+def test_register_with_invalid_input(client, user_name, password, message):
+    client.post(
+        '/authentication/register',
+        data={'user_name': 'fmercury', 'password': 'Test#6^0'}
+    )   # create existing user
+    response = client.post(
+        '/authentication/register',
+        data={'user_name': user_name, 'password': password}
+    )
+    assert message in response.data
+
+
+def test_login(client, auth):
+    # Check that we can retrieve the login page.
+    status_code = client.get('/authentication/login').status_code
+    assert status_code == 200
+
+    # Check that a successful login generates a redirect to the homepage.
+    client.post(
+        '/authentication/register',
+        data={'user_name': 'username', 'password': 'Password1'}
+    )   # create existing user
+    response = auth.login()
+    assert response.headers['Location'] == 'http://localhost/'
+
+    # Check that a session has been created for the logged-in user.
+    with client:
+        client.get('/')
+        assert session['user_name'] == 'username'
+
+
+def test_logout(client, auth):
+    # Login a user.
+    auth.login()
+
+    with client:
+        # Check that logging out clears the user's session.
+        auth.logout()
+        assert 'user_id' not in session
+
+
