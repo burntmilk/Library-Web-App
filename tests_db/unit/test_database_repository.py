95fddaf36e55069ed5909701469347935ed3197c
burntmilk <lukey1321@hotmail.com>
2021-10-15 14:59:56 +1300

    put correct code in test_populate_database.py

--- tests_db/unit/test_database_repository.py
+++ tests_db/unit/test_database_repository.py
@@ -1,49 +1,10 @@
-from sqlalchemy import select, inspect
-from library.adapters.orm import metadata
+from datetime import date, datetime
+
+import pytest
+
+import library.adapters.repository as repo
+from library.adapters.database_repository import SqlAlchemyRepository
+from library.domain.model import Publisher, Author, Book, Review, User, BooksInventory
+from library.adapters.repository import RepositoryException
 
 
-def test_database_populate_inspect_table_names(database_engine):
-    inspector = inspect(database_engine)
-    assert inspector.get_table_names() == ['authors', 'book_authors', 'books', 'favourites', 'publishers', 'reviews', 'users']
-
-
-def test_database_populate_select_all_authors(database_engine):
-    inspector = inspect(database_engine)
-    name_of_authors_table = inspector.get_table_names()[0]
-
-    with database_engine.connect() as connection:
-        select_statement = select([metadata.tables[name_of_authors_table]])
-        result = connection.execute(select_statement)
-        all_author_ids = []
-        for row in result:
-            all_author_ids.append(row['id'])
-        print(all_author_ids)
-        assert len(all_author_ids) == 31
-
-
-def test_database_populate_select_all_books(database_engine):
-    inspector = inspect(database_engine)
-    name_of_books_table = inspector.get_table_names()[2]
-
-    with database_engine.connect() as connection:
-        select_statement = select([metadata.tables[name_of_books_table]])
-        result = connection.execute(select_statement)
-        all_book_ids = []
-        for row in result:
-            all_book_ids.append(row['id'])
-        print(all_book_ids)
-        assert len(all_book_ids) == 20
-
-
-def test_database_populate_select_all_publishers(database_engine):
-    inspector = inspect(database_engine)
-    name_of_publishers_table = inspector.get_table_names()[4]
-
-    with database_engine.connect() as connection:
-        select_statement = select([metadata.tables[name_of_publishers_table]])
-        result = connection.execute(select_statement)
-        all_publisher_ids = []
-        for row in result:
-            all_publisher_ids.append(row['id'])
-        print(all_publisher_ids)
-        assert len(all_publisher_ids) == 12
